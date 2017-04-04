30ee4734debc1af941fc751435e0bedfbb5b1d06
SharpAceX <sharpacex@gmail.com>
2017-04-04 01:00:25 -0400

    Populated default config values

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,18 @@
 var Config = {};
 
+Config.setup_download_link = "https://www.google.com/";
+Config.client_download_link = "https://www.google.com/";
+
+Config.forum_link = "https://www.google.com/";
+
+Config.exp_rate = 1;
+Config.meso_rate = 1;
+Config.drop_rate = 1;
+
+Config.facebook_username = "aria";
+Config.twitter_handle = "@aria"
+Config.discord_server_id = 0;
+
 Config.base_url = "http://jsonplaceholder.typicode.com/";
 
 module.exports = Config;
