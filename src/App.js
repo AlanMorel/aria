06f188ff4f6dd6f07b4212dd078d1ea145fb0f68
SharpAceX <sharpacex@gmail.com>
2017-04-10 00:34:18 -0400

    Empty footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import Header from './components/navigation/header/Header';
+import Footer from './components/footer/Footer';
 import Post from './components/post/Post';
 import News from './components/news/News';
 import Rankings from './components/rankings/Rankings';
@@ -28,6 +29,7 @@ const App = () => (
       <Route exact path="/join" component={Join}/>
       <Route exact path="/downloads" component={Downloads}/>
       <Route exact path="/vote" component={Vote}/>
+      <Footer/>
     </div>
   </Router>
 );
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="footer">
+            </footer>
+        );
+    }
+}
+
+export default Footer;
