4c7bdde03859589333e28f9b5158239ad92f6f9d
SharpAceX <sharpacex@gmail.com>
2017-04-19 21:20:37 -0400

    Footer work and pagination color change

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,6 +4,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer className="footer">
+                This is where footer text goes.
             </footer>
         );
     }
