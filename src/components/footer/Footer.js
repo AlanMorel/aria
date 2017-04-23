29c369430f48af765c7aa0b4e5516f1f48db357d
SharpAceX <sharpacex@gmail.com>
2017-04-23 18:51:01 -0400

    Added more content and styles to the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Config from '../../Config';
 
 class Footer extends React.Component {
     render() {
         return (
             <footer className="footer">
-                This is where footer text goes.
+                <div className="copyright">Copyright &copy; 2017 {Config.server_name}. All rights reserved.</div>
+                <div className="credits">Aria built by <a href="http://alanmorel.com/" target="_blank">Alan Morel</a> and Austin.</div>
             </footer>
         );
     }
