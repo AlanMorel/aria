48912e831cb5a1626e8c9ddb787fdc4f41eae3a4
SharpAceX <sharpacex@gmail.com>
2017-04-23 21:20:47 -0400

    Category base added

---
+++ src/components/navigation/category/Category.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Category extends React.Component {
+
+    render() {
+      return (
+        <div className="Category">
+          <ul>
+
+          </ul>
+      </div>
+      );
+    }
+}
+
+export default Category;
