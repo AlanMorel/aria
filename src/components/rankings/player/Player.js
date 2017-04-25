05343eebfeba14bc1e01a954cffe1ecb3513d5e8
SharpAceX <sharpacex@gmail.com>
2017-04-25 18:27:57 -0400

    Player exp added

--- src/components/rankings/player/Player.js
+++ src/components/rankings/player/Player.js
@@ -12,7 +12,8 @@ class Player extends React.Component {
                     <img src={"https://playorion.net/characters/" + ign + ".png"} alt=""/>
                 </div>
                 <h2>{this.props.player.name}</h2>
-                <h3>Level {this.props.player.level} {Config.job[this.props.player.job_id].name}</h3>
+                <h3><span className="level">Level {this.props.player.level}</span> ({this.props.player.exp} exp)</h3>
+                <h3>{Config.job[this.props.player.job_id].name}</h3>
                 <h3>{this.props.player.fame} Fame</h3>
                 <h3 className="guild">{this.props.player.guild_name} Guild</h3>
             </section>
