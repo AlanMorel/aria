af5db3b88bca7b5bb9d50fcb15413d67e9f31561
SharpAceX <sharpacex@gmail.com>
2017-04-26 22:56:31 -0400

    Moved jobs to separate file

--- src/components/rankings/player/Player.js
+++ src/components/rankings/player/Player.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Config from '../../../Config';
+import Jobs from '../../../Jobs';
 
 class Player extends React.Component {
 
@@ -13,7 +14,7 @@ class Player extends React.Component {
                 </div>
                 <h2><span className="rank">#{this.props.rank}</span> {this.props.player.name}</h2>
                 <h3><span className="level">Level {this.props.player.level}</span> ({this.props.player.exp} exp)</h3>
-                <h3>{Config.job[this.props.player.job_id].name}</h3>
+                <h3>{Jobs[this.props.player.job_id].name}</h3>
                 <h3>{this.props.player.fame} Fame</h3>
                 <h3 className="guild">{this.props.player.guild_name} Guild</h3>
             </section>
