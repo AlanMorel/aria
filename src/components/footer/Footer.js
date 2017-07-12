44035b9d6d4449ad6ac18231d1a2058af8e48429
Austin Adams <holthelper@users.noreply.github.com>
2017-07-12 10:56:17 -0700

    Updated personal website link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
         return (
             <footer className="footer">
                 <div className="copyright">Copyright &copy; 2017 {Config.server_name}. All rights reserved.</div>
-                <div className="credits">Aria built by <a href="http://alanmorel.com/" target="_blank">Alan Morel</a> and <a href="http://austin-adams.us/" target="_blank">Austin Adams</a>.</div>
+                <div className="credits">Aria built by <a href="http://alanmorel.com/" target="_blank">Alan Morel</a> and <a href="http://austinradams.com/" target="_blank">Austin Adams</a>.</div>
             </footer>
         );
     }
